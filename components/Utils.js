9d0bd3b9356b9ee2d2b8881b928f042c90614791
AbdulazizYas <abdulazizyass@gmail.com>
2022-07-02 17:30:52 +0300

    design the modal  remaining the body

---
+++ components/Utils.js
@@ -0,0 +1,10 @@
+import styles from "../styles/Utils.module.css";
+
+export const Cross = () => {
+  return (
+    <>
+      <div className={styles.line1}></div>
+      <div className={styles.line2}></div>
+    </>
+  );
+};
--- components/tasmeat/ModalTasmee.js
+++ components/tasmeat/ModalTasmee.js
@@ -1,5 +1,55 @@
+import { Cross } from "../utils";
+
 const ModalTasmee = ({ isShown, setIsShown }) => {
-  return <div>bruh</div>;
+  return (
+    <>
+      {isShown && (
+        <div className="modal">
+          <div className="modalWindow">
+            <div className="header">
+              <span className="title">اضافة ختمة</span>
+              <button
+                style={{
+                  background: "none",
+                  boxShadow: "none",
+                  position: "relative",
+                  left: ".5rem",
+                }}
+                role="close"
+                onClick={() => setIsShown(false)}
+              >
+                <Cross />
+              </button>
+            </div>
+            <hr
+              style={{
+                margin: 0,
+                opacity: 0.25,
+                borderRadius: "50px",
+              }}
+            />
+            <div className="body">bruh</div>
+
+            <hr
+              style={{
+                margin: 0,
+                marginBottom: "1rem",
+                opacity: 0.25,
+                borderRadius: "50px",
+              }}
+            />
+            <div className="footer">
+              <button className="primary" style={{ width: "80%" }}>
+                إضافة
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {!isShown && <div></div>}
+    </>
+  );
 };
 
 export default ModalTasmee;
