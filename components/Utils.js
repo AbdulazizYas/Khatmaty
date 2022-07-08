2f0c5097c02df2ca6baf8e74701ce377923ff5b4
AbdulazizYas <abdulazizyass@gmail.com>
2022-07-08 15:07:20 +0300

    fix Utils comps

--- components/Utils.js
+++ components/Utils.js
@@ -1,49 +1,46 @@
 import { forwardRef, useState } from "react";
 import styles from "../styles/Utils.module.css";
 
-const Cross = () => {
+export function Cross() {
   return (
     <>
       <div className={styles.line1}></div>
       <div className={styles.line2}></div>
     </>
   );
-};
+}
 
-const Counter = forwardRef(({ id }, ref) => {
-  const [value, setValue] = useState(0);
-  return (
-    <div className={styles.inputWrapper}>
-      <button
-        type="button"
-        onClick={() => setValue((prev) => prev + 1)}
-        className={`${styles.btn}`}
-      >
-        +
-      </button>
+export function Counter() {
+  return forwardRef(({ id }, ref) => {
+    const [value, setValue] = useState(0);
+    return (
+      <div className={styles.inputWrapper}>
+        <button
+          type="button"
+          onClick={() => setValue((prev) => prev + 1)}
+          className={`${styles.btn}`}
+        >
+          +
+        </button>
 
-      <input
-        className={styles.counter}
-        min={0}
-        name={id}
-        id={id}
-        type="number"
-        ref={ref}
-        onChange={(e) => setValue(e.target.value)}
-        value={value || 0}
-      />
-      <button
-        type="button"
-        onClick={() => setValue((prev) => Math.max(0, prev - 1))}
-        className={`${styles.btn}`}
-      >
-        -
-      </button>
-    </div>
-  );
-});
-
-module.exports = {
-  Cross,
-  Counter,
-};
+        <input
+          className={styles.counter}
+          min={0}
+          name={id}
+          id={id}
+          type="number"
+          ref={ref}
+          onChange={(e) => setValue(e.target.value)}
+          value={value || 0}
+        />
+        <button
+          type="button"
+          onClick={() => setValue((prev) => Math.max(0, prev - 1))}
+          className={`${styles.btn}`}
+        >
+          -
+        </button>
+      </div>
+    );
+  });
+}
