bb017d7d779a043a0088476b1898734d1bb2a7eb
AbdulazizYas <abdulazizyass@gmail.com>
2022-07-08 15:19:01 +0300

    try to fix again 2

--- components/Utils.js
+++ components/Utils.js
@@ -1,11 +1,17 @@
 import { forwardRef, useState } from "react";
 import styles from "../styles/Utils.module.css";
 
-export function Cross() {
+const Cross = () => {
   return (
     <>
       <div className={styles.line1}></div>
       <div className={styles.line2}></div>
     </>
   );
-}
+};
+
+Cross.displayName = "Cross";
+
+module.exports = {
+  Cross,
+};
